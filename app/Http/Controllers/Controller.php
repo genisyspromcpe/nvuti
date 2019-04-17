5b82cfd6009ba8146ae528c67d2200da25729973
toxanutiy <toxanutiy@gmail.com>
2019-04-17 20:19:32 +0300

    Global user

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Config;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Auth;
+use Illuminate\Support\Facades\Redis;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $user;
+    protected $redis;
+
+    public function __construct()
+    {
+        $this->redis = Redis::connection();
+        $this->middleware(function ($request, $next) {
+            $this->user = Auth::user();
+            view()->share('u', $this->user);
+            return $next($request);
+        });
+    }
 }
