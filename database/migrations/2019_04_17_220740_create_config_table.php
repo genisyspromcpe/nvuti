b67ada4b73a52d5a8b9adb7e20f09ac0c831bedc
toxanutiy <toxanutiy@gmail.com>
2019-04-17 23:29:12 +0300

    Create 2019_04_17_220740_create_config_table.php
    Создал таблицу с конфигом

---
+++ database/migrations/2019_04_17_220740_create_config_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->double('profit', 8,2);
+            $table->double('need_profit', 8,2);
+            $table->integer('chance_win');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('config');
+    }
+}
