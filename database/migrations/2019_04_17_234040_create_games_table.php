b8346cf7c359bad2cd39ea881bc45b89a015bc3e
toxanutiy <toxanutiy@gmail.com>
2019-04-17 23:50:08 +0300

    Добавил таблицу games

---
+++ database/migrations/2019_04_17_234040_create_games_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('betType');
+            $table->integer('win_number');
+            $table->integer('bet');
+            $table->integer('win');
+            $table->string('game');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
