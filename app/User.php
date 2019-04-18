f9746c4a6b030971cf9da410465dfc8a5c4e2c0a
toxanutiy <toxanutiy@gmail.com>
2019-04-18 17:07:31 +0300

    Update User.php
    Добавил строку ip

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'balance', 'vkId', 'password', 'game', 'ref'
+        'username', 'email', 'balance', 'vkId', 'password', 'game', 'ref', 'ip'
     ];
 
     /**
