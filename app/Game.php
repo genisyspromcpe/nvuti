d2a1a44fdeed6347e675bb4fee9743a8918ab0cb
toxanutiy <toxanutiy@gmail.com>
2019-04-18 18:14:59 +0300

    Update Game.php
    Так же добавил строку betPercent

--- app/Game.php
+++ app/Game.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     protected $fillable = [
-        'user_id', 'betType', 'win_number', 'bet', 'win', 'game'
+        'user_id', 'betType', 'win_number', 'bet', 'win', 'game', 'betPercent'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
