c79436c6f1d64dedd769c6edea99402697a87f62
toxanutiy <toxanutiy@gmail.com>
2019-04-18 16:41:43 +0300

    Update User.php
    Добавил строку ref

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'balance', 'vkId', 'password', 'game'
+        'username', 'email', 'balance', 'vkId', 'password', 'game', 'ref'
     ];
 
     /**
