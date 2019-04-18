8ba572ced7a95f6bc45f0bdda989e4b5d2b97538
toxanutiy <toxanutiy@gmail.com>
2019-04-18 17:07:43 +0300

    Update AuthController.php
    При регистрации записывает ip

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -103,7 +103,8 @@ class AuthController extends Controller
                'username' => $r->login,
                'password' => md5($r->pass),
                'email' => $r->email,
-               'ref' => $referred
+               'ref' => $referred,
+               'ip' => request()->ip()
             ]);
 
             GameController::setHash($user);
@@ -151,7 +152,8 @@ class AuthController extends Controller
                 'username' => $data['first_name'].' '.$data['last_name'],
                 'email' => $user->accessTokenResponseBody['email'],
                 'vkId' => $data['id'],
-                'ref' => $referred
+                'ref' => $referred,
+                'ip' => $this->getIp()
             ]);
         }
 
@@ -165,5 +167,18 @@ class AuthController extends Controller
     {
         Auth::logout();
         return redirect('/');
+    }
+
+    public function getIp(){
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true){
+                foreach (explode(',', $_SERVER[$key]) as $ip){
+                    $ip = trim($ip); // just to be safe
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return $ip;
+                    }
+                }
+            }
+        }
     }
 }
