531e74a6a939184194a0e01cd6ee32b0c168b476
toxanutiy <toxanutiy@gmail.com>
2019-04-18 16:36:42 +0300

    Update PagesController.php
    Добавил проверку на реферальную ссылку

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,8 +7,12 @@ use Auth;
 
 class PagesController extends Controller
 {
-    public function index()
+    public function index(Request $r)
     {
+        if(isset($r->i))
+        {
+            $r->session()->put('ref', $r->i);
+        }
         if (Auth::check())
         {
             return view('logged');
